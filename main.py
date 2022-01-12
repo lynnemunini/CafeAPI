fd07c266416e33f10363809787c8f16c088933d4
Lynne Munini <muninilynne65@gmail.com>
2022-01-12 13:29:52 -0500

    Get all Cafes

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
 
 @app.route("/")
 def home():
@@ -36,9 +40,33 @@ def home():
 def get_random_cafe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    return jsonify(id=random_cafe.id, name=random_cafe.name, map_url=random_cafe.map_url, img_url=random_cafe.img_url, location=random_cafe.location,
-    seats=random_cafe.seats, has_toilet=random_cafe.has_toilet, has_wifi=random_cafe.has_wifi, has_sockets=random_cafe.has_sockets, can_take_calls=random_cafe.can_take_calls,
-    coffee_price=random_cafe.coffee_price)
+    # return jsonify(cafe={
+    #     id=random_cafe.id, 
+    #     name=random_cafe.name, 
+    #     map_url=random_cafe.map_url, 
+    #     img_url=random_cafe.img_url, 
+    #     location=random_cafe.location,
+    #     seats=random_cafe.seats, 
+    #     has_toilet=random_cafe.has_toilet, 
+    #     has_wifi=random_cafe.has_wifi, 
+    #     has_sockets=random_cafe.has_sockets, 
+    #     can_take_calls=random_cafe.can_take_calls,
+    #     coffee_price=random_cafe.coffee_price})
+
+    #Simply convert the random_cafe data record to a dictionary of key-value pairs. 
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all")
+def all():
+    cafes = db.session.query(Cafe).all()
+    data = []
+    for cafe in cafes:
+        cafe_dict = cafe.to_dict()
+        data.append(cafe_dict)
+
+    return jsonify(cafes=data)
+
 
 ## HTTP POST - Create Record
 
